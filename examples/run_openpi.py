e7ececa1f747f3d28514d59b699c91a1699af8fd
Arsh Tangri <atangri@path-robotics.com>
2025-12-23 16:15:27 -0500

    New Running Example

--- examples/run_openpi.py
+++ examples/run_openpi.py
@@ -1,25 +1,34 @@
 from openpi.training import config as _config
 from openpi.policies import policy_config
-from openpi.shared import download
-from openpi.policies import droid_policy
+import openpi.training.data_loader as _data_loader
 
+from lerobot.common.datasets.lerobot_dataset import (
+    LeRobotDataset,
+    LeRobotDatasetMetadata)
 
-config = _config.get_config("pi05_droid")
-checkpoint_dir = "/home/path/models/openpi/pi05_droid_pytorch"
+config = _config.get_config("pi0_ladderclip_finetune")
+checkpoint_dir = "/home/path/Desktop/openpi/checkpoints/pi0_ladderclip_finetune/10000"
 print("Checkpoint dir:", checkpoint_dir)
 
 
 # Create a trained policy.
 policy = policy_config.create_trained_policy(config, checkpoint_dir)
+print("Policy created:", policy)
 
-# print("Policy created:", policy)
 
+# Load LeRobot dataset
+data_dir = "/home/path/Desktop/robot-learning/data/ladder_clip_18"
+metadata = meta0 = LeRobotDatasetMetadata("", root=data_dir)  # any repo_id will do
+dataset = LeRobotDataset(repo_id="",root=data_dir)
 
-# Run inference on a dummy example. This example corresponds to observations produced by the DROID runtime.
-example = droid_policy.make_droid_example()
-result = policy.infer(example)
+sample = dataset[0]
 
-# Delete the policy to free up memory.
-del policy
+sample['actions'] = sample['actions'][None, :]  # Add batch dimension
+sample['observation.low_dim_obs'] = sample['observation.low_dim_obs'][None, :]  # Add batch dimension
+sample['observation.images.third_view_rgb'] = sample['observation.images.third_view_rgb'][None, ...]  # Add batch dimension
+sample['observation.images.wrist_1_rgb'] = sample['observation.images.wrist_1_rgb'][None, ...]  # Add batch dimension
+sample['observation.images.wrist_2_rgb'] = sample['observation.images.wrist_2_rgb'][None, ...]  # Add batch dimension
 
-print("Actions shape:", result["actions"].shape) 
+# Run the policy on a sample.
+action = policy.infer(sample)
+print("Inferred action Shape:", action['actions'].shape)
